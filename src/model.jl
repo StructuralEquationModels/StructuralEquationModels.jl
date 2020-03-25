bd6d52a6c20ff75ad1a71912f8feba977498e82b
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2020-03-25 16:50:54 +0100

    rewrite functions to use new model system

--- src/model.jl
+++ src/model.jl
@@ -1,11 +1,12 @@
-function model(ram, data, par, est = ML, optim = "LBFGS")
+function model(ram, data, par, est = ML, opt = "LBFGS")
     data_matr = convert(Matrix{Float64}, data)
 
     model = Dict{Symbol, Any}(
         :ram => ram,
         :par => par,
         :data => data_matr,
-        :optim => optim,
+        :opt => opt,
+        :est => est,
         :obs_cov => missing,
         :imp_cov => missing,
         :obs_mean => missing,
@@ -13,22 +14,4 @@ function model(ram, data, par, est = ML, optim = "LBFGS")
         :opt_result => missing
       )
       return model
-end
-
-function ram(x)
-      S =   [x[1] 0 0 0
-            0 x[2] 0 0
-            0 0 x[3] 0
-            0 0 0 x[4]]
-
-      F =  [1 0 0 0
-            0 1 0 0
-            0 0 1 0]
-
-      A =  [0 0 0 1
-            0 0 0 x[5]
-            0 0 0 x[6]
-            0 0 0 0]
-
-      return (S, F, A)
 end
