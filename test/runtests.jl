8d19194c072a927124099148411511d307e5e146
Aaron Peikert <aaron.peikert@posteo.de>
2020-03-25 16:56:36 +0100

    add basic tests for model spec

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,49 +1,46 @@
-using sem, Test#, Feather
+using sem, Test, Feather
 
-## Test one factor model with Holzinger_Swineford data
+function holz_onef_mod(x)
+      S =   [x[1] 0 0 0
+            0 x[2] 0 0
+            0 0 x[3] 0
+            0 0 0 x[4]]
 
-# import results from lavaan
-#holz_onef_par = Feather.read("test/comparisons/holz_onef_par.feather")
-#holz_onef_dat = Feather.read("test/comparisons/holz_onef_dat.feather")
+      F =  [1 0 0 0
+            0 1 0 0
+            0 0 1 0]
 
-#function holz_onef_mod(x)
-#      S =   [x[1] 0 0 0
-#            0 x[2] 0 0
-#            0 0 x[3] 0
-#            0 0 0 x[4]]
-#
-#      F =  [1 0 0 0
-#            0 1 0 0
-#            0 0 1 0]
-#
-#      A =  [0 0 0 1
-#            0 0 0 x[5]
-#            0 0 0 x[6]
-#            0 0 0 0]
-#
-#      return (S, F, A)
-#end
+      A =  [0 0 0 1
+            0 0 0 x[5]
+            0 0 0 x[6]
+            0 0 0 0]
+
+      return (S, F, A)
+end
+
+holz_onef_dat = Feather.read("test/comparisons/holz_onef_dat.feather")
 
 
-#x0 = append!([0.5, 0.5, 0.5, 0.5], ones(2))
+@testset "test model spec" begin
+    mymod = sem.model(holz_onef_mod, holz_onef_dat, [0.5, 0.5, 0.5, 0.5, 1.0, 1.0])
+    @test all([i in [
+            :ram
+            :par
+            :data
+            :optim
+            :obs_cov
+            :imp_cov
+            :obs_mean
+            :logl
+            :opt_result] for i = keys(mymod)])
+      @test mymod[:data] == convert(Matrix{Float64}, holz_onef_dat)
+      @test mymod[:ram] == holz_onef_mod
+    #sem.sem_obs_cov(mymod)
 
-#holz_onef_fit_lb = fit_sem(holz_onef_mod, holz_onef_dat, x0, ML, "LBFGS")
+    #sem.sem_imp_cov(mymod)
 
-#holz_onef_fit_ne = fit_sem(holz_onef_mod, holz_onef_dat, x0, ML, "Newton")
+    #sem_obs_mean(mymod)
 
-holz_onef_dat =
-      Feather.read("test/comparisons/holz_onef_dat.feather")
+    #sem_logl(mymod)
 
-mymod = model(ram, holz_onef_dat, [0.5, 0.5, 0.5, 0.5, 1.0, 1.0])
-
-sem_obs_cov(mymod)
-
-sem_imp_cov(mymod)
-
-sem_obs_mean(mymod)
-
-sem_logl(mymod)
-
-@testset "sem.jl" begin
-    #@test x0 == [0.5, 0.5, 0.5, 0.5, 1.0, 1.0]
 end
