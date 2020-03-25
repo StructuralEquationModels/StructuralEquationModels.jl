4adf2e6d491ec9be943550d335561f1bfdd70b57
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2020-03-25 12:16:51 +0100

    include more files

--- src/sem.jl
+++ src/sem.jl
@@ -1,5 +1,11 @@
 module sem
 
+include("sem_wrapper_functions.jl")
+include("objective_function.jl")
+include("helper_functions.jl")
+
 greet() = print("Hello World!")
+
+export optim_sem
 
 end # module
