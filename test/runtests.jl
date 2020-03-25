cac86d12509859420cf4b122ed3ffd7573e07d0e
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2020-03-25 13:28:36 +0100

    test test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
 using sem
 using Test
 
+function ram(x)
+      S =   [x[1] 0 0 0
+            0 x[2] 0 0
+            0 0 x[3] 0
+            0 0 0 x[4]]
+
+      F =  [1 0 0 0
+            0 1 0 0
+            0 0 1 0]
+
+      A =  [0 0 0 1
+            0 0 0 x[5]
+            0 0 0 x[6]
+            0 0 0 0]
+
+      return (S, F, A)
+end
+
+
+x0 = append!([0.5, 0.5, 0.5, 0.5], ones(2))
+
 @testset "sem.jl" begin
-    # Write your own tests here.
+    x0 == [0.5, 0.5, 0.5, 0.5, 1.0, 1.0]
 end
