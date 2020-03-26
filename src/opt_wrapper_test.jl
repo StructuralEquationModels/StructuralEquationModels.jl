36d716c4e81cabfbd414ebc8c7c1018862215541
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2020-03-26 22:13:39 +0100

    add "*_test.jl" files with functions that pass dictionaries to the objective

---
+++ src/opt_wrapper_test.jl
@@ -0,0 +1,18 @@
+# wrapper to call the optimizer
+function opt_sem(model)
+      if model[:opt] == "LBFGS"
+            objective = parameters ->
+                  model[:est](parameters, ;model...)
+            result =
+                  optimize(objective, model[:par], LBFGS(),
+                        autodiff = :forward)
+      elseif model[:opt] == "Newton"
+            objective = TwiceDifferentiable(
+                  parameters -> model[:est](parameters, ;model...),
+                        model[:par],
+                        autodiff = :forward)
+                  result = optimize(objective, model[:par])
+      else
+            error("Unknown Optimizer")
+      end
+end
