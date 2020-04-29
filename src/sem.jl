d7c057febeaf2d366faf4961603d72060c1d7ef4
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2020-04-29 20:05:42 +0200

    reset master

--- src/sem.jl
+++ src/sem.jl
@@ -3,13 +3,8 @@ module sem
 using Distributions, Feather, ForwardDiff, LinearAlgebra, Optim, Random,
     NLSolversBase
 
-include("opt_wrapper_test.jl")
-include("objective_test.jl")
-include("helper.jl")
-include("exported.jl")
 include("model.jl")
 
-export sem_fit!, model, delta_method!, sem_imp_cov!, sem_obs_mean!,
-    sem_logl!, sem_est!, sem_opt!
+export
 
 end # module
