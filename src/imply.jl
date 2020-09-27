ca170dc72a6ed1d5977df2214a7624057d035777
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2020-09-27 13:51:23 +0200

    add ImplySymbolic and tests

--- src/imply.jl
+++ src/imply.jl
@@ -13,8 +13,49 @@ struct ImplyDense{A <: Array{Float64}} <: Imply
     implied::A
 end
 
-struct ImplySymbolic{A} <: Imply
-    implied::A
+struct ImplySymbolic{F <: Any, A <: AbstractArray} <: Imply
+    imp_fun::F
+    imp_cov::A
+end
+
+function ImplySymbolic(
+        A::Spa1,
+        S::Spa2,
+        F::Spa3,
+        parameters,
+        start_val
+            ) where {
+            Spa1 <: SparseMatrixCSC,
+            Spa2 <: SparseMatrixCSC,
+            Spa3 <: SparseMatrixCSC
+            }
+
+    invia = I + A
+    next_term = A^2
+
+    while nnz(next_term) != 0
+        invia += next_term
+        next_term *= next_term
+    end
+
+    imp_cov_sym = F*invia*S*permutedims(invia)*permutedims(F)
+
+    imp_cov_sym = Array(imp_cov_sym)
+    imp_cov_sym .= simplify.(imp_cov_sym)
+
+    imp_fun_, imp_fun =
+        ModelingToolkit.build_function(
+            imp_cov_sym,
+            parameters,
+            expression=Val{false}
+        )
+    
+    imp_cov = imp_fun_(start_val)
+    return ImplySymbolic(imp_fun, imp_cov)
+end
+
+function(imply::ImplySymbolic)(parameters)
+    imply.imp_fun(imply.imp_cov, parameters)
 end
 
 function (imply::ImplySparse)(par)
