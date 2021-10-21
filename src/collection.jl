966ac18178fb6d421e6ca72ea86596511f3507dc
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2021-10-21 16:27:44 +0200

    delete old files

--- src/collection.jl
+++
@@ -1,9 +0,0 @@
-function CollectionSem(observed, imply, loss, diff)
-
-    observed = make_onelement_array(observed)
-    imply = make_onelement_array(imply)
-    loss = make_onelement_array(loss)
-    diff = make_onelement_array(diff)
-
-    return CollectionSem(observed, imply, loss, diff)
-end
