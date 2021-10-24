7bfcb43891afad5be52b33841ea2893b47961407
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2021-10-24 02:51:20 +0200

    delete old files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,3 @@
 using Test, SafeTestsets
 
-@test 1==1
-
-#@time @safetestset "Example Models" begin include("examples/example_tests.jl") end
+@time @safetestset "Example Models" begin include("examples/example_tests.jl") end
