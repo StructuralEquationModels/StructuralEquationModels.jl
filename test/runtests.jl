805c4f661fe41172fcda9aff42b647f95d8f9df2
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2021-12-01 11:09:42 +0100

    add unit tests infrastructure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
 using Test, SafeTestsets
 
+@time @safetestset "Unit Tests" begin include("unit_tests/unit_tests.jl") end
 @time @safetestset "Example Models" begin include("examples/example_tests.jl") end
