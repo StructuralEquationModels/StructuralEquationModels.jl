65e35de898401bf26768ae1fb447bfebe162315e
maximilian <maximilian.stefan.ernst@gmail.com>
2022-02-11 21:33:52 +0100

    update forwarddiff constrcutor

--- src/sem.jl
+++ src/sem.jl
@@ -114,3 +114,56 @@ function SemFiniteDiff(;
     return sem
 
 end
+
+function SemForwardDiff(;
+        observed::O = SemObsCommon,
+        imply::I = RAM,
+        loss::L = (SemML,),
+        diff::D = SemDiffOptim,
+        has_gradient = false,
+        kwargs...) where {O, I, L, D}
+
+    kwargs = Dict{Symbol, Any}(kwargs...)
+
+    kwargs[:observed_type] = O <: Type ? observed : typeof(observed)
+    kwargs[:imply_type] = I <: Type ? imply : typeof(imply)
+    kwargs[:loss_types] = [lossfun isa SemLossFunction ? typeof(lossfun) : lossfun for lossfun in loss]
+    kwargs[:diff_type] = D <: Type ? diff : typeof(diff)
+
+    if O <: Type
+        observed = observed(;kwargs...)
+    end
+
+    kwargs[:observed] = observed
+
+    if !isa(imply, SemImply)
+        imply = imply(;kwargs...)
+    end
+
+    kwargs[:imply] = imply
+    kwargs[:n_par] = length(imply.start_val)
+
+    loss_out = []
+
+    for lossfun in loss
+        if isa(lossfun, SemLossFunction)
+            append!(loss_out, [lossfun])
+        else
+            lossfun = lossfun(;kwargs...)
+            push!(loss_out, lossfun)
+        end
+    end
+
+    loss = SemLoss((loss_out...,))
+
+    kwargs[:loss] = loss
+
+    if !isa(diff, SemDiff)
+        diff = diff(;kwargs...)
+    end
+
+    sem = SemForwardDiff(observed, imply, loss, diff, has_gradient)
+
+    return sem
+
+end
