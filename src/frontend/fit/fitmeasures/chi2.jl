9146ebba8f83aada90347180fdc07a12c8575189
maximilian <maximilian.stefan.ernst@gmail.com>
2022-03-02 17:08:02 +0100

    start adding fit measures

---
+++ src/frontend/fit/fitmeasures/chi2.jl
@@ -0,0 +1,25 @@
+#####################################################################################################
+# Single Models
+#####################################################################################################
+
+# SemFit splices loss functions ---------------------------------------------------------------------
+χ²(sem_fit::SemFit{Mi, S, Mo, O} where {Mi, S, Mo <: AbstractSemSingle, O}) = 
+    χ²(
+        sem_fit,
+        sem_fit.model.observed,
+        sem_fit.model.imply,
+        sem_fit.model.diff,
+        sem_fit.model.loss.functions...
+        )
+
+# RAM + SemML
+χ²(sem_fit::SemFit, obs, imp::RAM, diff, loss_ml::SemML) = χ²(sem_fit.minimum, obs)
+χ²(sem_fit::SemFit, obs, imp::RAMSymbolic, diff, loss_ml::SemML) = χ²(sem_fit.minimum, obs)
+
+
+# generic SemObsCommon
+χ²(minimum, observed::SemObsCommon) = (observed.n_obs-1)*Fₘᵢₙ(minimum, observed.obs_cov, observed.n_man)
+
+#####################################################################################################
+# Collections
+#####################################################################################################
