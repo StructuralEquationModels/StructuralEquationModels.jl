0a0dc1a0471e3ce433e18e994c9e16de0449031a
maximilian <maximilian.stefan.ernst@gmail.com>
2022-03-03 20:42:38 +0100

    add to fit measures

--- src/frontend/fit/fitmeasures/BIC.jl
+++ src/frontend/fit/fitmeasures/BIC.jl
@@ -14,7 +14,7 @@ BIC(sem_fit::SemFit{Mi, S, Mo, O} where {Mi, S, Mo <: AbstractSemSingle, O}) =
 
 # RAM + SemML
 BIC(sem_fit::SemFit, obs, imp::Union{RAM, RAMSymbolic}, diff, loss_ml::SemML) =
-    BIC(sem_fit.minimum, obs.n_man, obs.n_obs, npar(imp))
+    minus2ll(sem_fit) + log(ob.n_obs)*npar(imp)
 
 function BIC(minimum, n_man, n_obs, n_par)
     BIC = minus2ll(minimum, n_obs, n_man) + log(n_obs)*n_par
