2a7de1b6879ce88f20cf4465805d1d423671d283
maximilian <maximilian.stefan.ernst@gmail.com>
2022-03-05 22:43:34 +0100

    change starting values interface

---
+++ src/additional_functions/start_val/start_partable.jl
@@ -0,0 +1,36 @@
+# splice model and loss functions
+function start_parameter_table(model::Union{Sem, SemForwardDiff, SemFiniteDiff}; kwargs...)
+    return start_parameter_table(
+        model.observed, 
+        model.imply,
+        model.diff, 
+        model.loss.functions...,
+        kwargs...)
+end
+
+# RAM(Symbolic)
+function start_parameter_table(observed, imply::Union{RAM, RAMSymbolic}, diff, args...; kwargs...)
+    return start_parameter_table(
+        imply.ram_matrices;
+        kwargs...)
+end
+
+function start_parameter_table(ram_matrices::RAMMatrices; parameter_table::ParameterTable, kwargs...)
+    
+    start_val = zeros(0)
+    
+    for identifier_ram in ram_matrices.identifier
+        found = false
+        for (i, identifier_table) in enumerate(parameter_table.identifier)
+            if identifier_ram == identifier_table
+                push!(start_val, parameter_table.start[i])
+                found = true
+                break
+            end
+        end
+        if !found @error "At least one parameter could not be found in the parameter table." end
+    end
+
+    return start_val
+
+end
