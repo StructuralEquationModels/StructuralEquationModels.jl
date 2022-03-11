ab4d0652815cbff328275549337dce61891a5409
maximilian <maximilian.stefan.ernst@gmail.com>
2022-03-11 13:35:15 +0100

    remove proximal algorithms

--- src/optimizer/Proximal.jl
+++
@@ -1,7 +0,0 @@
-function sem_fit(model::Sem{O, I, L, D}) where {O, I, L, D <: SemDiffProximal}
-
-    solver = model.diff.algorithm
-    minimizer, num_it = solver(model.imply.start_val, f = model, g = model.diff.g)
-
-    return minimizer, num_it
-end
