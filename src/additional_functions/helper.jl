e39e1f819dc6cd4970825de2aea2acddc998ba62
maximilian <maximilian.stefan.ernst@gmail.com>
2022-03-19 14:07:52 +0100

    add helper functions to get parameter indices

--- src/additional_functions/helper.jl
+++ src/additional_functions/helper.jl
@@ -299,3 +299,16 @@ function commutation_matrix_pre_square_add_mt!(B, A) # comuptes B + KₙA # 0 al
     return B
 
 end
+
+function get_parameter_indices(parameters, model_parameters)
+    indices = [findfirst(x -> x == par, model_parameters) for par in parameters]
+    return indices
+end
+
+get_parameter_indices(parameters, specification::RAMMatrices) = get_parameter_indices(parameters, specification.parameters)
+
+function get_parameter_indices(parameters, specification::ParameterTable)
+    model_parameters = unique(specification.identifier)
+    filter!(x -> x != :const, model_parameters)
+    return get_parameter_indices(model_parameters, parameters)
+end
