334474d3dfe2b8031b9291cc7f82c76a27c7545d
maximilian <maximilian.stefan.ernst@gmail.com>
2022-03-20 16:04:15 +0100

    finish rework of NLopt interface and test it

--- src/diff/NLopt.jl
+++ src/diff/NLopt.jl
@@ -2,17 +2,35 @@
 ### Types
 ############################################################################
 
-struct SemDiffNLopt{A, B} <: SemDiff
+struct SemDiffNLopt{A, A2, B, B2, C} <: SemDiff
     algorithm::A
+    local_algorithm::A2
     options::B
-    equality_constraints
-    inequality_constraints
+    local_options::B2
+    equality_constraints::C
+    inequality_constraints::C
 end
 
-function SemDiffNLopt(;algorithm = :LD_LBFGS, options = nothing, equality_constraints = [], inequality_constraints = [], kwargs...)
+Base.@kwdef mutable struct NLoptConstraint
+    f
+    tol = 0.0
+end
+
+############################################################################
+### Constructor
+############################################################################
+
+function SemDiffNLopt(;
+        algorithm = :LD_LBFGS,
+        local_algorithm = nothing, 
+        options = Dict{Symbol, Any}(),
+        local_options = Dict{Symbol, Any}(), 
+        equality_constraints = Vector{NLoptConstraint}(), 
+        inequality_constraints = Vector{NLoptConstraint}(), 
+        kwargs...)
     applicable(iterate, equality_constraints) || (equality_constraints = [equality_constraints])
-    applicable(iterate, inequality_constraints) || (equality_constraints = [equality_constraints])
-    return SemDiffNLopt(algorithm, options, equality_constraints, inequality_constraints)
+    applicable(iterate, inequality_constraints) || (inequality_constraints = [inequality_constraints])
+    return SemDiffNLopt(algorithm, local_algorithm, options, local_options, equality_constraints, inequality_constraints)
 end
 
 ############################################################################
@@ -24,11 +42,3 @@ function Base.show(io::IO, struct_inst::SemDiffNLopt)
     print_field_types(io, struct_inst)
 end
 
-############################################################################
-### Options
-############################################################################
-
-Base.@kwdef mutable struct NLoptConstraint
-    f
-    tol = 0.0
-end
--- src/frontend/fit/SemFit.jl
+++ src/frontend/fit/SemFit.jl
@@ -9,3 +9,18 @@ mutable struct SemFit{Mi, So, St, Mo, O}
     model::Mo
     optimization_result::O
 end
+
+##############################################################
+# pretty printing
+##############################################################
+
+function Base.show(io::IO, semfit::SemFit)
+    print(io, "Fitted Structural Equation Model \n")
+    print(io, "================================ \n")
+    print(io, "------------- Model ------------ \n")
+    print(io, semfit.model)
+    print(io, "\n")
+    #print(io, "Objective value: $(round(semfit.minimum, digits = 4)) \n")
+    print(io, "----- Optimization result ------ \n")
+    print(io, semfit.optimization_result)
+end
