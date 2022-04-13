2cc177400e77d8d1689dc34ace4ee2a7b336895e
maximilian <maximilian.stefan.ernst@gmail.com>
2022-04-13 22:56:14 +0200

    clean code

--- src/additional_functions/artifacts.jl
+++ src/additional_functions/artifacts.jl
@@ -1,8 +1,5 @@
 # load an example data set
 function example_data(name)
-    #print(@artifact_str name)
-    #hash = artifact_hash(name, joinpath(replace(@__DIR__, "additional_functions" => ""), "Artifacts.toml"))
-    #basepath = artifact_path(hash)
     basepath = @artifact_str name
     path = readdir(basepath, join = true)[1]
     data, header = readdlm(path, ',', header = true)
