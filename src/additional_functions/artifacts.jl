2dc0624c0c99d04f00b7cc3622116acc77104394
maximilian <maximilian.stefan.ernst@gmail.com>
2022-04-22 17:55:47 +0200

    update artifact helper

--- src/additional_functions/artifacts.jl
+++ src/additional_functions/artifacts.jl
@@ -1,9 +1,23 @@
 # load an example data set
 function example_data(name)
     basepath = @artifact_str name
-    path = readdir(basepath, join = true)[1]
-    data, header = readdlm(path, ',', header = true)
-    data_raw = DataFrame(data, vec(header))
-    data = identity.(data_raw)
-    return data
+    paths = readdir(basepath)
+    if length(paths) == 1
+        path = joinpath(basepath, paths[1])
+        data, header = readdlm(path, ',', header = true)
+        data_raw = DataFrame(data, vec(header))
+        data = identity.(data_raw)
+        return data
+    else
+        out = Dict()
+        for path in paths
+            key = Symbol(split(path, ".")[1])
+            path = joinpath(basepath, path)
+            data, header = readdlm(path, ',', header = true)
+            data_raw = DataFrame(data, vec(header))
+            data = identity.(data_raw)
+            push!(out, key => data)
+        end
+        return out
+    end
 end
