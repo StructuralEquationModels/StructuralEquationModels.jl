7f078dca4e53df151d7e38df6cb6d3d97ce00c9d
maximilian <maximilian.stefan.ernst@gmail.com>
2022-04-25 01:29:01 +0200

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,25 +7,29 @@ makedocs(
     pages = [
         "index.md",
         "Tutorials" => [
-            "Fit a SEM" => [
-                "tutorials/high_level.md",
-                "tutorials/build_by_parts.md"],
+            "A first model" => "tutorials/first_model.md",
+            "Model specification" => [],
+            "Model construction" => ["tutorials/construction/build_by_parts.md"],
+            "Model fitting" => [],
+            "Model inspection" => [],
             "Multigroup SEM" => [
-                "tutorials/multigroup.md",
-                "tutorials/collection.md"
-            ],
-            "Custom loss functions" => "tutorials/loss.md",
-            "Custom imply types" => "tutorials/imply.md",
-            "Custom sem types" => "tutorials/sem.md"
+                "tutorials/multigroup/multigroup.md",
+                "tutorials/multigroup/collection.md"
+            ]
+        ],
+        "Developer documentation" => [
+            "Custom loss functions" => "developer/loss.md",
+            "Custom imply types" => "developer/imply.md",
+            "Custom sem types" => "developer/sem.md"
         ],
     ],
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     ),
-    doctest = :fix
+    doctest = false
 )
 
-doctest(MyPackage, fix=true)
+doctest(StructuralEquationModels, fix=true)
 
 deploydocs(
     repo = "github.com/StructuralEquationModels/StructuralEquationModels.jl",
