a77c51d2d7c29aba0c25c7c99646b0f4fba9ce0c
maximilian <maximilian.stefan.ernst@gmail.com>
2022-05-19 10:07:39 +0200

    add extended tests environment variable

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test, SafeTestsets
 
+@test ENV["JULIA_EXTENDED_TESTS"] == "true"
 @time @safetestset "Unit Tests" begin include("unit_tests/unit_tests.jl") end
 @time @safetestset "Example Models" begin include("examples/examples.jl") end
