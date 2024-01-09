ccc937df030213557defc11858b70ccd16e53aa9
Maximilian-Stefan-Ernst <34346372+Maximilian-Stefan-Ernst@users.noreply.github.com>
2024-01-09 17:42:13 +0100

    Update some test tolerances

--- test/examples/political_democracy/by_parts.jl
+++ test/examples/political_democracy/by_parts.jl
@@ -252,7 +252,7 @@ end
 
     update_partable!(partable_mean, identifier(model_ml), se_hessian(solution_ml), :se)
     @test compare_estimates(partable_mean, solution_lav[:parameter_estimates_ml_mean]; 
-        atol = 1e-3, col = :se, lav_col = :se)
+        atol = 0.002, col = :se, lav_col = :se)
 end
 
 @testset "fitmeasures/se_ls_mean" begin
