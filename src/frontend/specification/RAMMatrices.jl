6ce98c33f8a0c07ec538768327271b0cb06ab625
Alexey Stukalov <astukalol@seer.bio>
2024-03-09 15:24:39 -0800

    RAMMatrices: optimize F_indices init

--- src/frontend/specification/RAMMatrices.jl
+++ src/frontend/specification/RAMMatrices.jl
@@ -68,7 +68,7 @@ function RAMMatrices(;A, S, F, M = nothing, parameters, colnames)
     A_indices = array_parameters_map_linear(parameters, A)
     S_indices = array_parameters_map_linear(parameters, S)
     M_indices = !isnothing(M) ? array_parameters_map_linear(parameters, M) : nothing
-    F_indices = findall([any(isone.(col)) for col in eachcol(F)])
+    F_indices = [i for (i, col) in zip(axes(F, 2), eachcol(F)) if any(isone, col)]
     constants = Vector{RAMConstant}()
     append_RAMConstants!(constants, :A, A)
     append_RAMConstants!(constants, :S, S)
