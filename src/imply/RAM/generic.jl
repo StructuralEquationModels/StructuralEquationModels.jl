c69f2629bd792e41616ca21f20356bf26f5a556d
Alexey Stukalov <astukalol@seer.bio>
2024-03-10 13:46:53 -0700

    use ternary op as intended

--- src/imply/RAM/generic.jl
+++ src/imply/RAM/generic.jl
@@ -115,7 +115,7 @@ function RAM(;
     # get indices
     A_indices = copy(ram_matrices.A_ind)
     S_indices = copy(ram_matrices.S_ind)
-    !isnothing(ram_matrices.M_ind) ? M_indices = copy(ram_matrices.M_ind) : M_indices = nothing
+    M_indices = !isnothing(ram_matrices.M_ind) ? copy(ram_matrices.M_ind) : nothing
 
     #preallocate arrays
     A_pre = zeros(n_var, n_var)
