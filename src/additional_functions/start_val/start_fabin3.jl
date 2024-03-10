e6fde8c15022091545c5fe3a1a537cc6228bf501
Alexey Stukalov <astukalol@seer.bio>
2024-03-09 18:39:48 -0800

    start_fabin3: optimize indexing
    * use sorted searches
    * use dict to map indicator to param index

--- src/additional_functions/start_val/start_fabin3.jl
+++ src/additional_functions/start_val/start_fabin3.jl
@@ -66,10 +66,8 @@ function start_fabin3(ram_matrices::RAMMatrices, Σ, μ)
     C_indices = CartesianIndices((n_nod, n_nod))
 
     # check in which matrix each parameter appears
-    
-    indices = Vector{CartesianIndex}(undef, n_par)
 
-    start_val = zeros(n_par)
+    indices = Vector{CartesianIndex{2}}(undef, n_par)
 
 #=     in_S = length.(S_ind) .!= 0
     in_A = length.(A_ind) .!= 0
@@ -89,16 +87,12 @@ function start_fabin3(ram_matrices::RAMMatrices, Σ, μ)
 
     # set undirected parameters in S
     for (i, S_ind) in enumerate(S_ind)
-        if length(S_ind) != 0 
-            c_ind = C_indices[S_ind][1]
-            if c_ind[1] == c_ind[2]
-                if c_ind[1] ∈ F_ind
-                    index_position = findall(c_ind[1] .== F_ind)
-                    start_val[i] = Σ[index_position[1], index_position[1]]/2
-                else
-                    start_val[i] = 0.05
-                end
-            end # covariances stay 0
+        for c_ind in C_indices[S_ind]
+            (c_ind[1] == c_ind[2]) || continue # covariances stay 0
+            pos = searchsortedfirst(F_ind, c_ind[1])
+            start_val[i] = (pos <= length(F_ind)) && (F_ind[pos] == c_ind[1]) ?
+                Σ[pos, pos]/2 : 0.05
+            break # i-th parameter initialized
         end
     end
 
@@ -107,76 +101,78 @@ function start_fabin3(ram_matrices::RAMMatrices, Σ, μ)
     A_ind_c = [linear2cartesian(ind, (n_nod, n_nod)) for ind in A_ind]
     # ind_Λ = findall([is_in_Λ(ind_vec, F_ind) for ind_vec in A_ind_c])
 
-    for i ∈ findall(.!(1:n_nod .∈ [F_ind]))
+    for i ∈ setdiff(1:n_nod, F_ind)
         reference = Int64[]
         indicators = Int64[]
-        loadings = Symbol[]
+        indicator2parampos = Dict{Int, Int}()
 
-        for (j, ind_c_vec) in enumerate(A_ind_c)
-            for ind_c in ind_c_vec
-                if (ind_c[2] == i) & (ind_c[1] ∈ F_ind)
-                    push!(indicators, ind_c[1])
-                    push!(loadings, parameters[j])
+        for (j, Aj_ind_c) in enumerate(A_ind_c)
+            for ind_c in Aj_ind_c
+                (ind_c[2] == i) || continue
+                ind_pos = searchsortedfirst(F_ind, ind_c[1])
+                if (ind_pos <= length(F_ind)) && (F_ind[ind_pos] == ind_c[1])
+                    push!(indicators, ind_pos)
+                    indicator2parampos[ind_pos] = j
                 end
             end
         end
 
-        for ram_constant in constants
-            if (ram_constant.matrix == :A) && (ram_constant.index[2] == i) && (ram_constant.index[1] ∈ F_ind)
-                push!(loadings, Symbol(""))
-                if isone(ram_constant.value)
-                    push!(reference, ram_constant.index[1])
-                else
-                    push!(indicators, ram_constant.index[1])
+        for ram_const in constants
+            if (ram_const.matrix == :A) && (ram_const.index[2] == i)
+                ind_pos = searchsortedfirst(F_ind, ram_const.index[1])
+                if (ind_pos <= length(F_ind)) && (F_ind[ind_pos] == ram_const.index[1])
+                    if isone(ram_const.value)
+                        push!(reference, ind_pos)
+                    else
+                        push!(indicators, ind_pos)
+                        # no parameter associated
+                    end
                 end
             end
         end
-
-        reference = [findfirst(x -> x == ref, F_ind) for ref in reference]
-        indicators = [findfirst(x -> x == ind, F_ind) for ind in indicators]
 
         # is there at least one reference indicator?
-        if size(reference, 1) > 0
-            if size(reference, 1) > 1
-                @warn "You have more than 1 scaling indicator"
-                reference = reference[1]
+        if length(reference) > 0
+            if length(reference) > 1
+                if isempty(indicator2parampos) # don't warn if entire column is fixed
+                    @warn "You have more than 1 scaling indicator for $(ram_matrices.colnames[i])"
+                end
+                ref = reference[1]
             else
-                reference = reference[1]
+                ref = reference[1]
             end
 
             for (j, indicator) in enumerate(indicators)
-                if indicator != reference 
-                    instruments = indicators[.!(indicators .∈ [[reference; indicator]])]
+                if indicator != ref
+                    instruments = filter(i -> (i != ref) && (i != indicator), indicators)
 
                     s32 = Σ[instruments, indicator]
-                    s13 = Σ[reference, instruments]
+                    s13 = Σ[ref, instruments]
                     S33 = Σ[instruments, instruments]
-                    
+
                     if size(instruments, 1) == 1
                         temp = S33[1]/s13[1]
                         λ = s32[1]*temp/(s13[1]*temp)
-                        start_val[isequal.(parameters, loadings[j])] .= λ
                     else
                         temp = S33\s13
                         λ = s32'*temp/(s13'*temp)
-                        start_val[isequal.(parameters, loadings[j])] .= λ
                     end
-
+                    start_val[indicator2parampos[indicator]] = λ
                 end
             end
         # no reference indicator:
-        else
-            reference = indicators[1]
-            λ = zeros(size(indicators, 1)); λ[1] = 1.0
+        elseif length(indicators) > 0
+            ref = indicators[1]
+            λ = Vector{Float64}(undef, length(indicators)); λ[1] = 1.0
             for (j, indicator) in enumerate(indicators)
-                if indicator != reference 
-                    instruments = indicators[.!(indicators .∈ [[reference; indicator]])]
+                if indicator != ref
+                    instruments = filter(i -> (i != ref) && (i != indicator), indicators)
 
                     s32 = Σ[instruments, indicator]
-                    s13 = Σ[reference, instruments]
+                    s13 = Σ[ref, instruments]
                     S33 = Σ[instruments, instruments]
                     
-                    if size(instruments, 1) == 1
+                    if length(instruments) == 1
                         temp = S33[1]/s13[1]
                         λ[j] = s32[1]*temp/(s13[1]*temp)
                     else
@@ -199,19 +195,23 @@ function start_fabin3(ram_matrices::RAMMatrices, Σ, μ)
             λ = λ .* sign(Ψ) .* sqrt(abs(Ψ))
 
             for (j, indicator) ∈ enumerate(indicators)
-                start_val[isequal.(parameters, loadings[j])] .= λ[j]
+                if (parampos = get(indicator2parampos, indicator, 0)) != 0
+                    start_val[parampos] = λ[j]
+                end
             end
+        else
+            @warn "No scaling indicators for $(ram_matrices.colnames[i])"
         end
     end
 
     # set means
     if !isnothing(M_ind)
         for (i, M_ind) in enumerate(M_ind)
-            if length(M_ind) != 0 
+            if length(M_ind) != 0
                 ind = M_ind[1]
-                if ind[1] ∈ F_ind
-                    index_position = findfirst(ind[1] .== F_ind)
-                    start_val[i] = μ[index_position]
+                pos = searchsortedfirst(F_ind, ind[1])
+                if (pos <= length(F_ind)) && (F_ind[pos] == ind[1])
+                    start_val[i] = μ[pos]
                 end # latent means stay 0
             end
         end
@@ -221,6 +221,5 @@ function start_fabin3(ram_matrices::RAMMatrices, Σ, μ)
 end
 
 function is_in_Λ(ind_vec, F_ind)
-    res = [!(ind[2] ∈ F_ind) & (ind[1] ∈ F_ind) for ind in ind_vec]
-    return any(res)
+    return any(ind -> !(ind[2] ∈ F_ind) && (ind[1] ∈ F_ind), ind_vec)
 end
