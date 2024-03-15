7563e2091fd00de7bc37bb592974b94e57b87b06
Alexey Stukalov <astukalov@gmail.com>
2024-03-15 08:36:35 -0700

    tests: use approx op

--- test/examples/recover_parameters/recover_parameters_twofact.jl
+++ test/examples/recover_parameters/recover_parameters_twofact.jl
@@ -74,4 +74,4 @@ model_ml = Sem(semobserved, imply_ml, loss_ml, optimizer)
 objective!(model_ml, true_val)
 solution_ml = sem_fit(model_ml)
 
-@test isapprox(true_val, solution(solution_ml); atol = 0.05)
+@test true_val ≈ solution(solution_ml) atol = 0.05
