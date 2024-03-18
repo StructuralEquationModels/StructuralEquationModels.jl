98b74d9cb7f1d38ac3a54921e4917338005ab9e3
Alexey Stukalov <astukalov@gmail.com>
2024-03-17 17:19:57 -0700

    include RAMMatrices before EnsParTable

--- src/StructuralEquationModels.jl
+++ src/StructuralEquationModels.jl
@@ -32,8 +32,8 @@ include("frontend/fit/SemFit.jl")
 # specification of models
 include("frontend/specification/checks.jl")
 include("frontend/specification/ParameterTable.jl")
-include("frontend/specification/EnsembleParameterTable.jl")
 include("frontend/specification/RAMMatrices.jl")
+include("frontend/specification/EnsembleParameterTable.jl")
 include("frontend/specification/StenoGraphs.jl")
 include("frontend/fit/summary.jl")
 # pretty printing
