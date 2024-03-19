ee0f27391bb5bf9b871e59092a47cf1ccac63716
Alexey Stukalov <astukalol@seer.bio>
2024-03-18 23:50:04 -0700

    fix RMSEA

--- src/frontend/fit/fitmeasures/RMSEA.jl
+++ src/frontend/fit/fitmeasures/RMSEA.jl
@@ -5,11 +5,13 @@ Return the RMSEA.
 """
 function RMSEA end
 
-RMSEA(sem_fit::SemFit{Mi, So, St, Mo, O} where {Mi, So, St, Mo <: AbstractSemSingle, O}) = 
+RMSEA(sem_fit::SemFit) = RMSEA(sem_fit, sem_fit.model)
+
+RMSEA(sem_fit::SemFit, model::AbstractSemSingle) =
     RMSEA(df(sem_fit), χ²(sem_fit), n_obs(sem_fit))
 
-RMSEA(sem_fit::SemFit{Mi, So, St, Mo, O} where {Mi, So, St, Mo <: SemEnsemble, O}) = 
-    sqrt(length(sem_fit.model.sems))*RMSEA(df(sem_fit), χ²(sem_fit), n_obs(sem_fit))
+RMSEA(sem_fit::SemFit, model::SemEnsemble) =
+    sqrt(length(model.sems))*RMSEA(df(sem_fit), χ²(sem_fit), n_obs(sem_fit))
 
 function RMSEA(df, chi2, n_obs)
     rmsea = (chi2 - df) / (n_obs*df)
