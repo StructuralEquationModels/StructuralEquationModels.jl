1c376a585e656d8fe5f46bad9cd7da02cf9aa89e
Alexey Stukalov <astukalol@seer.bio>
2024-03-20 12:24:56 -0700

    WLS: use 5-arg mul!()
    to reduce allocations

--- src/loss/WLS/WLS.jl
+++ src/loss/WLS/WLS.jl
@@ -127,8 +127,7 @@ function evaluate!(
         end
         gradient .*= -2
     end
-    isnothing(hessian) || (mul!(hessian, ∇σ' * V, ∇σ);
-    hessian .*= 2)
+    isnothing(hessian) || (mul!(hessian, ∇σ' * V, ∇σ, 2, 0))
     if !isnothing(hessian) && (HessianEvaluation(semwls) === ExactHessian)
         ∇²Σ_function! = implied.∇²Σ_function
         ∇²Σ = implied.∇²Σ
@@ -145,7 +144,7 @@ function evaluate!(
             objective += dot(μ₋, V_μ, μ₋)
         end
         if !isnothing(gradient)
-            gradient .-= 2 * (μ₋' * V_μ * implied.∇μ)'
+            mul!(gradient, (V_μ * implied.∇μ)', μ₋, -2, 1)
         end
     end
 
