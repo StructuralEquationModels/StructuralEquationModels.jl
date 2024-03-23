30343ce6e71b95554a8f67b66faa947b1e1e7f98
Alexey Stukalov <astukalol@seer.bio>
2024-03-23 16:01:51 -0700

    param_values(ParTable)

--- src/frontend/specification/ParameterTable.jl
+++ src/frontend/specification/ParameterTable.jl
@@ -300,3 +300,53 @@ function update_se_hessian!(
     se = se_hessian(sem_fit; hessian = hessian)
     return update_partable!(partable, sem_fit, se, :se)
 end
+
+"""
+    param_values!(out::AbstractVector, partable::ParameterTable,
+                  col::Symbol = :estimate)
+
+Extract parameter values from the `col` column of `partable`
+into the `out` vector.
+
+The `out` vector should be of `nparams(partable)` length.
+The *i*-th element of the `out` vector will contain the
+value of the *i*-th parameter from `params(partable)`.
+
+Note that the function combines the duplicate occurences of the
+same parameter in `partable` and will raise an error if the
+values do not match.
+"""
+function param_values!(out::AbstractVector, partable::ParameterTable,
+                       col::Symbol = :estimate)
+    (length(out) == nparams(partable)) ||
+        throw(DimensionMismatch("The length of parameter values vector ($(length(out))) does not match the number of parameters ($(nparams(partable)))"))
+    param_index = Dict(param => i for (i, param) in enumerate(params(partable)))
+    param_values_col = partable.columns[col]
+    for (i, param) in enumerate(partable.columns.param)
+        (param == :const) && continue
+        param_ind = get(param_index, param, nothing)
+        @assert !isnothing(param_ind) "Parameter table contains unregistered parameter :$param at row #$i"
+        val = param_values_col[i]
+        if !isnan(out[param_ind])
+            @assert out[param_ind] ≈ val atol=1E-10 "Parameter :$param value at row #$i ($val) differs from the earlier encountered value ($(out[param_ind]))"
+        else
+            out[param_ind] = val
+        end
+    end
+    return out
+end
+
+"""
+    param_values(out::AbstractVector, col::Symbol = :estimate)
+
+Extract parameter values from the `col` column of `partable`.
+
+Returns the values vector. The *i*-th element corresponds to
+the value of *i*-th parameter from `params(partable)`.
+
+Note that the function combines the duplicate occurences of the
+same parameter in `partable` and will raise an error if the
+values do not match.
+"""
+param_values(partable::ParameterTable, col::Symbol = :estimate) =
+    param_values!(fill(NaN, nparams(partable)), partable, col)
