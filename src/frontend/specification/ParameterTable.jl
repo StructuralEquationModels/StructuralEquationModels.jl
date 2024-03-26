aaee5b7db45ed6beff9f7594dace6bfe4cf22258
Alexey Stukalov <astukalol@seer.bio>
2024-03-25 23:50:32 -0700

    variance_params(SEMSpec)

--- src/frontend/specification/ParameterTable.jl
+++ src/frontend/specification/ParameterTable.jl
@@ -392,6 +392,18 @@ function update_se_hessian!(
     return update_partable!(partable, :se, params(fit), se)
 end
 
+function variance_params(partable::ParameterTable)
+    res = [
+        param for (param, rel, from, to) in zip(
+            partable.columns.param,
+            partable.columns.relation,
+            partable.columns.from,
+            partable.columns.to,
+        ) if (rel == :↔) && (from == to)
+    ]
+    unique!(res)
+end
+
 """
     param_values!(out::AbstractVector, partable::ParameterTable,
                   col::Symbol = :estimate)
