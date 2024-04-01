a3f7e865d599af11f351b3f384b0256b89bb2a75
Alexey Stukalov <astukalol@seer.bio>
2024-04-01 10:06:52 -0700

    u/l_bounds support for Optim.jl

--- src/optimizer/optim.jl
+++ src/optimizer/optim.jl
@@ -22,13 +22,38 @@ function sem_fit(
     optim::SemOptimizerOptim,
     model::AbstractSem,
     start_params::AbstractVector;
+    lower_bounds::Union{AbstractVector, AbstractDict, Nothing} = nothing,
+    upper_bounds::Union{AbstractVector, AbstractDict, Nothing} = nothing,
+    variance_lower_bound::Float64 = 0.0,
+    lower_bound = -Inf,
+    upper_bound = Inf,
     kwargs...,
 )
-    result = Optim.optimize(
-        Optim.only_fgh!((F, G, H, par) -> evaluate!(F, G, H, model, par)),
-        start_params,
-        model.optimizer.algorithm,
-        model.optimizer.options,
-    )
+
+    # setup lower/upper bounds if the algorithm supports it
+    if optim.algorithm isa Optim.Fminbox || optim.algorithm isa Optim.SAMIN
+        lbounds = SEM.lower_bounds(
+            lower_bounds,
+            model,
+            default = lower_bound,
+            variance_default = variance_lower_bound,
+        )
+        ubounds = SEM.upper_bounds(upper_bounds, model, default = upper_bound)
+        result = Optim.optimize(
+            Optim.only_fgh!((F, G, H, par) -> evaluate!(F, G, H, model, par)),
+            lbounds,
+            ubounds,
+            start_params,
+            model.optimizer.algorithm,
+            model.optimizer.options,
+        )
+    else
+        result = Optim.optimize(
+            Optim.only_fgh!((F, G, H, par) -> evaluate!(F, G, H, model, par)),
+            start_params,
+            model.optimizer.algorithm,
+            model.optimizer.options,
+        )
+    end
     return SemFit(result, model, start_params)
 end
