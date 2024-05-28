7eef27e6380db300be7aadfec752ed55d1dad128
Alexey Stukalov <astukalol@seer.bio>
2024-05-28 16:16:04 -0700

    p_values(): use ccdf()

--- src/frontend/fit/fitmeasures/p.jl
+++ src/frontend/fit/fitmeasures/p.jl
@@ -3,4 +3,4 @@
 
 Return the p value computed from the χ² test statistic.
 """
-p_value(sem_fit::SemFit) = 1 - cdf(Chisq(df(sem_fit)), χ²(sem_fit))
+p_value(sem_fit::SemFit) = ccdf(Chisq(df(sem_fit)), χ²(sem_fit))
