5abc9838bba6254b8f0564ed24926d322cb70ef3
Alexey Stukalov <alyst@users.noreply.github.com>
2024-08-11 12:08:58 -0700

    SemObservedCov: def as an alias of SemObservedData
    reduces code duplication;
    also annotate types of ctor args
    now samples(SemObsCov) returns nothing

--- src/observed/covariance.jl
+++ src/observed/covariance.jl
@@ -1,65 +1,21 @@
 """
-For observed covariance matrices and means.
-
-# Constructor
-
-    SemObservedCovariance(;
-        specification,
-        obs_cov,
-        obs_colnames = nothing,
-        meanstructure = false,
-        obs_mean = nothing,
-        nsamples = nothing,
-        kwargs...)
-
-# Arguments
-- `specification`: either a `RAMMatrices` or `ParameterTable` object (1)
-- `obs_cov`: observed covariance matrix
-- `obs_colnames::Vector{Symbol}`: column names of the covariance matrix
-- `meanstructure::Bool`: does the model have a meanstructure?
-- `obs_mean`: observed mean vector
-- `nsamples::Number`: number of samples (observed data points); necessary for fit statistics
-
-# Extended help
-## Interfaces
-- `nsamples(::SemObservedCovariance)`: number of samples (observed data points)
-- `n_man(::SemObservedCovariance)` -> number of manifest variables
-
-- `obs_cov(::SemObservedCovariance)` -> observed covariance matrix
-- `obs_mean(::SemObservedCovariance)` -> observed means
-
-## Implementation
-Subtype of `SemObserved`
-
-## Remarks
-(1) the `specification` argument can also be `nothing`, but this turns of checking whether
-the observed data/covariance columns are in the correct order! As a result, you should only
-use this if you are sure your covariance matrix is in the right format.
-
-## Additional keyword arguments:
-- `spec_colnames::Vector{Symbol} = nothing`: overwrites column names of the specification object
+Type alias for [SemObservedData](@ref) with no data, but with mean and covariance.
 """
-struct SemObservedCovariance{B, C} <: SemObserved
-    obs_cov::B
-    obs_mean::C
-    nobs_vars::Int
-    nsamples::Int
-end
+SemObservedCovariance{B, C} = SemObservedData{Nothing, B, C}
 
 function SemObservedCovariance(;
     specification::Union{SemSpecification, Nothing} = nothing,
-    obs_cov,
-    obs_colnames = nothing,
-    spec_colnames = nothing,
-    obs_mean = nothing,
-    meanstructure = false,
+    obs_cov::AbstractMatrix,
+    obs_colnames::Union{AbstractVector{Symbol}, Nothing} = nothing,
+    spec_colnames::Union{AbstractVector{Symbol}, Nothing} = nothing,
+    obs_mean::Union{AbstractVector, Nothing} = nothing,
+    meanstructure::Bool = false,
     nsamples::Integer,
     kwargs...,
 )
-    if !meanstructure & !isnothing(obs_mean)
+    if !meanstructure && !isnothing(obs_mean)
         throw(ArgumentError("observed means were passed, but `meanstructure = false`"))
-
-    elseif meanstructure & isnothing(obs_mean)
+    elseif meanstructure && isnothing(obs_mean)
         throw(ArgumentError("`meanstructure = true`, but no observed means were passed"))
     end
 
@@ -67,11 +23,8 @@ function SemObservedCovariance(;
         spec_colnames = observed_vars(specification)
     end
 
-    if !isnothing(spec_colnames) & isnothing(obs_colnames)
+    if !isnothing(spec_colnames) && isnothing(obs_colnames)
         throw(ArgumentError("no `obs_colnames` were specified"))
-
-    elseif !isnothing(spec_colnames) & !(eltype(obs_colnames) <: Symbol)
-        throw(ArgumentError("please specify `obs_colnames` as a vector of Symbols"))
     end
 
     if !isnothing(spec_colnames)
@@ -80,22 +33,5 @@ function SemObservedCovariance(;
         isnothing(obs_mean) || (obs_mean = obs_mean[obs2spec_perm])
     end
 
-    return SemObservedCovariance(obs_cov, obs_mean, size(obs_cov, 1), nsamples)
+    return SemObservedData(nothing, obs_cov, obs_mean, size(obs_cov, 1), nsamples)
 end
-
-############################################################################################
-### Recommended methods
-############################################################################################
-
-nsamples(observed::SemObservedCovariance) = observed.nsamples
-nobserved_vars(observed::SemObservedCovariance) = observed.nobs_vars
-
-samples(observed::SemObservedCovariance) =
-    error("$(typeof(observed)) does not store data samples")
-
-############################################################################################
-### additional methods
-############################################################################################
-
-obs_cov(observed::SemObservedCovariance) = observed.obs_cov
-obs_mean(observed::SemObservedCovariance) = observed.obs_mean
