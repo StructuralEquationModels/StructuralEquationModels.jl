8a98831cb10aa6f2287a2883d88b7a11f7dbc4de
Alexey Stukalov <alyst@users.noreply.github.com>
2024-12-19 16:22:27 -0800

    merge diff/*.jl optimizer code into optimizer/*.jl

--- ext/SEMProximalOptExt.jl
+++ ext/SEMProximalOptExt.jl
@@ -9,7 +9,6 @@ SEM = StructuralEquationModels
 
 #ProximalCore.prox!(y, f, x, gamma) = ProximalOperators.prox!(y, f, x, gamma)
 
-include("diff/Proximal.jl")
 include("optimizer/ProximalAlgorithms.jl")
 
 end
