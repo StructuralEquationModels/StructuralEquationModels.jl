eabb4ca8431af7a8a5fc1f114027e155b97be74f
Aaron Peikert <aaron.peikert@posteo.de>
2025-03-10 11:56:16 +0100

    remove unnessary test layer

--- test/unit_tests/StatsAPI.jl
+++ test/unit_tests/StatsAPI.jl
@@ -1,16 +1,15 @@
-@testset "StatsAPI" begin
-    using StructuralEquationModels
-    graph = @StenoGraph begin
-        a →  b
-    end
-    partable = ParameterTable(graph, observed_vars = [:a, :b], latent_vars = Symbol[])
-    update_partable!(partable, :estimate, param_labels(partable), [3.1415])
-    @testset "params" begin
-        out = [NaN]
-        StructuralEquationModels.params!(out, partable)
-        @test params(partable) == out == [3.1415]
-    end
-    @testset "param_labels" begin
-        @test param_labels(partable) == [:θ_1]
-    end
+using StructuralEquationModels
+graph = @StenoGraph begin
+    a → b
 end
+partable = ParameterTable(graph, observed_vars = [:a, :b], latent_vars = Symbol[])
+update_partable!(partable, :estimate, param_labels(partable), [3.1415])
+@testset "params" begin
+    out = [NaN]
+    StructuralEquationModels.params!(out, partable)
+    @test params(partable) == out == [3.1415]
+end
+@testset "param_labels" begin
+    @test param_labels(partable) == [:θ_1]
+end
+
