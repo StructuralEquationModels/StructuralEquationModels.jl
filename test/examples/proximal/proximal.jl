sem-jl/SEM.jl

test/Proximal: move usings to the central file

test/examples/proximal/proximal.jl
@@ -1,3 +1,5 @@
+using StructuralEquationModels, Test, ProximalCore, ProximalAlgorithms, ProximalOperators
+
 @testset "Ridge" begin
     include("ridge.jl")
 end

StructuralEquationModels/StructuralEquationModels.jl

test/examples/proximal/proximal.jl
@@ -1,3 +1,5 @@
+using StructuralEquationModels, Test, ProximalAlgorithms, ProximalOperators, Suppressor
+
 @testset "Ridge" begin
     include("ridge.jl")
 end