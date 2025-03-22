453fcc9ff81e1e81e3b746ea4695b5f1976e960b
Alexey Stukalov <alyst@users.noreply.github.com>
2025-03-21 21:00:35 -0700

    Revert "fix Proximal extension"
    This reverts commit 9729819b86f375e4663de1fe9ec9c38d4932f580.

--- ext/SEMProximalOptExt/SEMProximalOptExt.jl
+++ ext/SEMProximalOptExt/SEMProximalOptExt.jl
@@ -3,7 +3,8 @@ module SEMProximalOptExt
 using StructuralEquationModels
 using StructuralEquationModels: print_type_name, print_field_types
 using ProximalAlgorithms
-import StructuralEquationModels: SemOptimizerProximal
+
+export SemOptimizerProximal
 
 SEM = StructuralEquationModels
 
