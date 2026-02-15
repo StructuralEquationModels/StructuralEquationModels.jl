c63ca4c4e4a8babe75bcafc06897afb8c579310f
Maximilian Ernst <maximilian.stefan.ernst@gmail.com>
2026-02-15 18:33:16 +0100

    refactor RMSEA

--- src/frontend/fit/fitmeasures/RMSEA.jl
+++ src/frontend/fit/fitmeasures/RMSEA.jl
@@ -7,13 +7,20 @@ function RMSEA end
 
 RMSEA(fit::SemFit) = RMSEA(fit, fit.model)
 
-RMSEA(fit::SemFit, model::AbstractSemSingle) = RMSEA(dof(fit), χ²(fit), nsamples(fit))
+function RMSEA(fit::SemFit, model::AbstractSemSingle)
+    check_uniform_lossfun(model)
+    return RMSEA(dof(fit), χ²(fit), nsamples(fit)-dof_correction(model.loss.functions[1]))
+end
 
-RMSEA(fit::SemFit, model::SemEnsemble) =
-    sqrt(length(model.sems)) * RMSEA(dof(fit), χ²(fit), nsamples(fit))
+function RMSEA(fit::SemFit, model::SemEnsemble)
+    check_single_lossfun(model; throw_error = true)
+    n = nsamples(fit)-model.n*dof_correction(model.sems[1].loss.functions[1])
+    return sqrt(length(model.sems)) * RMSEA(dof(fit), χ²(fit), n)
+end
 
-function RMSEA(dof, chi2, nsamples)
-    rmsea = (chi2 - dof) / (nsamples * dof)
-    rmsea > 0 ? nothing : rmsea = 0
+function RMSEA(dof, chi2, c)
+    rmsea = (chi2 - dof) / (c * dof)
+    rmsea = rmsea > 0 ? rmsea : 0
     return sqrt(rmsea)
 end
+
