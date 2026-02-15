8d65c8016cb3d68736745f9e490f3dec7dae7b9f
Maximilian Ernst <maximilian.stefan.ernst@gmail.com>
2026-02-15 18:32:59 +0100

    refactor chi2

--- src/frontend/fit/fitmeasures/chi2.jl
+++ src/frontend/fit/fitmeasures/chi2.jl
@@ -16,20 +16,21 @@ with the *observed* covariance matrix.
 # Single Models
 ############################################################################################
 
-χ²(fit::SemFit, model::AbstractSemSingle) =
-    sum(loss -> χ²(loss, fit, model), model.loss.functions)
+function χ²(fit::SemFit, model::AbstractSemSingle)
+    check_single_lossfun(model; throw_error = true)
+    return χ²(model.loss.functions[1], fit::SemFit, model::AbstractSemSingle)
+end
 
-# RAM + SemML
-χ²(lossfun::SemML, fit::SemFit, model::AbstractSemSingle) =
+χ²(::SemML, fit::SemFit, model::AbstractSemSingle) =
     (nsamples(fit) - 1) *
     (fit.minimum - logdet(obs_cov(observed(model))) - nobserved_vars(observed(model)))
 
 # bollen, p. 115, only correct for GLS weight matrix
-χ²(lossfun::SemWLS, fit::SemFit, model::AbstractSemSingle) =
+χ²(::SemWLS, fit::SemFit, model::AbstractSemSingle) =
     (nsamples(fit) - 1) * fit.minimum
 
 # FIML
-function χ²(lossfun::SemFIML, fit::SemFit, model::AbstractSemSingle)
+function χ²(::SemFIML, fit::SemFit, model::AbstractSemSingle)
     ll_H0 = minus2ll(fit)
     ll_H1 = minus2ll(observed(model))
     return ll_H0 - ll_H1
@@ -39,38 +40,25 @@ end
 # Collections
 ############################################################################################
 
-function χ²(fit::SemFit, models::SemEnsemble)
-    isempty(models.sems) && return 0.0
-
-    lossfun = models.sems[1].loss.functions[1]
-    # check that all models use the same single loss function
-    L = typeof(lossfun)
-    for (i, sem) in enumerate(models.sems)
-        if length(sem.loss.functions) > 1
-            @error "Model for group #$i has $(length(sem.loss.functions)) loss functions. Only the single one is supported"
-        end
-        cur_lossfun = sem.loss.functions[1]
-        if !isa(cur_lossfun, L)
-            @error "Loss function for group #$i model is $(typeof(cur_lossfun)), expected $L. Heterogeneous loss functions are not supported"
-        end
-    end
-
-    return χ²(lossfun, fit, models)
+function χ²(fit::SemFit, model::SemEnsemble)
+    check_single_lossfun(model; throw_error = true)
+    lossfun = model.sems[1].loss.functions[1]
+    return χ²(lossfun, fit, model)
 end
 
-function χ²(lossfun::SemWLS, fit::SemFit, models::SemEnsemble)
-    return (nsamples(models) - 1) * fit.minimum
+function χ²(::SemWLS, fit::SemFit, models::SemEnsemble)
+    return (nsamples(models) - models.n) * fit.minimum
 end
 
-function χ²(lossfun::SemML, fit::SemFit, models::SemEnsemble)
+function χ²(::SemML, fit::SemFit, models::SemEnsemble)
     G = sum(zip(models.weights, models.sems)) do (w, model)
         data = observed(model)
         w * (logdet(obs_cov(data)) + nobserved_vars(data))
     end
-    return (nsamples(models) - 1) * (fit.minimum - G)
+    return (nsamples(models) - models.n) * (fit.minimum - G)
 end
 
-function χ²(lossfun::SemFIML, fit::SemFit, models::SemEnsemble)
+function χ²(::SemFIML, fit::SemFit, models::SemEnsemble)
     ll_H0 = minus2ll(fit)
     ll_H1 = sum(minus2ll ∘ observed, models.sems)
     return ll_H0 - ll_H1
