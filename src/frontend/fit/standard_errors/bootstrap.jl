1623119200299c0696d719b3a04590e987c12421
Maximilian Ernst <maximilian.stefan.ernst@gmail.com>
2026-03-09 23:07:35 +0100

    refactor bootstrap and add bootstrap for any statistic

--- src/frontend/fit/standard_errors/bootstrap.jl
+++ src/frontend/fit/standard_errors/bootstrap.jl
@@ -1,53 +1,78 @@
 """
-    se_bootstrap(
-        sem_fit::SemFit;
+    bootstrap(
+        fitted::SemFit;
+        statistic = solution,
         n_boot = 3000,
         data = nothing,
         specification = nothing,
+        engine = :Optim,
         parallel = false,
-        kwargs...)
+        fit_kwargs = Dict(),
+        replace_kwargs = Dict())
 
-Return bootstrap standard errors.
+Return bootstrap samples for `statistic`.
 
 # Arguments
+- `fitted`: a fitted SEM.
+- `statistic`: any function that can be called on a `SemFit` object.
+  The output will be returned as the bootstrap sample.
 - `n_boot`: number of boostrap samples
 - `data`: data to sample from. Only needed if different than the data from `sem_fit`
-- `specification`: a `ParameterTable` or `RAMMatrices` object passed down to `replace_observed`.
+- `specification`: a `ParameterTable` or `RAMMatrices` object passed to `replace_observed`.
    Necessary for FIML models.
+- `engine`: optimizer engine, passed to `fit`.
 - `parallel`: if `true`, run bootstrap samples in parallel on all available threads.
   The number of threads is controlled by the `JULIA_NUM_THREADS` environment variable or
   the `--threads` flag when starting Julia.
-- `kwargs...`: passed down to `replace_observed`
+- `fit_kwargs` : a `Dict` controlling model fitting for each bootstrap sample,
+  passed to `fit`
+- `replace_kwargs`: a `Dict` passed to `replace_observed`
+
+# Example
+```julia
+# 1000 boostrap samples of the minimum, fitted with :Optim
+bootstrap(
+    fitted;
+    statistic = StructuralEquationModels.minimum,
+    n_boot = 1000,
+    engine = :Optim,
+)
+```
 """
-function se_bootstrap(
-    fitted::SemFit{Mi, So, St, Mo, O};
+function bootstrap(
+    fitted::SemFit;
+    statistic = solution,
     n_boot = 3000,
     data = nothing,
     specification = nothing,
+    engine = :Optim,
     parallel = false,
-    kwargs...,
-) where {Mi, So, St, Mo, O}
+    fit_kwargs = Dict(),
+    replace_kwargs = Dict()
+)
     # access data and convert to matrix
     data = prepare_data_bootstrap(data, fitted.model)
     start = solution(fitted)
     # pre-allocations
-    total_sum         = zero(start)
-    total_squared_sum = zero(start)
-    n_failed          = Ref(0)
+    out = []
+    conv = []
+    n_failed = Ref(0)
     # fit to bootstrap samples
     if !parallel
         for _ in 1:n_boot
-            sample_data = bootstrap_sample(data)
-            new_model = replace_observed(
-                fitted.model;
-                data = sample_data,
-                specification = specification,
-                kwargs...,
-            )
             try
-                sol = solution(fit(new_model; start_val = start))
-                @. total_sum         += sol
-                @. total_squared_sum += sol^2
+                sample_data = bootstrap_sample(data)
+                new_model = replace_observed(
+                    fitted.model;
+                    data = sample_data,
+                    specification = specification,
+                    replace_kwargs...,
+                )
+                new_fit = fit(new_model; start_val = start, engine = engine, fit_kwargs...)
+                sample = statistic(new_fit)
+                c = converged(new_fit)
+                push!(out, sample)
+                push!(conv, c)
             catch
                 n_failed[] += 1
             end
@@ -69,12 +94,14 @@ function se_bootstrap(
                     thread_model;
                     data = sample_data,
                     specification = specification,
-                    kwargs...,
+                    replace_kwargs...,
                 )
-                sol = solution(fit(new_model; start_val = start))
+                new_fit = fit(new_model; start_val = start, engine = engine, fit_kwargs...)
+                sample = statistic(new_fit)
+                c = converged(new_fit)
                 lock(lk) do
-                    @. total_sum         += sol
-                    @. total_squared_sum += sol^2
+                    push!(out, sample)
+                    push!(conv, c)
                 end
             catch
                 lock(lk) do
@@ -86,13 +113,148 @@ function se_bootstrap(
         end
     end
     # compute parameters
-    n_conv = n_boot - n_failed[]
+    if !iszero(n_failed[])
+        @warn "During bootstrap sampling, "*string(n_failed[])*" samples errored."
+    end
+    return Dict(
+        :samples => out,
+        :n_boot => n_boot,
+        :n_converged => sum(conv),
+        :converged => conv,
+        :n_errored => n_failed[])
+end
+
+"""
+    se_bootstrap(
+        fitted::SemFit;
+        n_boot = 3000,
+        data = nothing,
+        specification = nothing,
+        engine = :Optim,
+        parallel = false,
+        fit_kwargs = Dict(),
+        replace_kwargs = Dict())
+
+Return bootstrap standard errors.
+
+# Arguments
+- `fitted`: a fitted SEM.
+- `n_boot`: number of boostrap samples
+- `data`: data to sample from. Only needed if different than the data from `sem_fit`
+- `specification`: a `ParameterTable` or `RAMMatrices` object passed to `replace_observed`.
+   Necessary for FIML models.
+- `engine`: optimizer engine, passed to `fit`.
+- `parallel`: if `true`, run bootstrap samples in parallel on all available threads.
+  The number of threads is controlled by the `JULIA_NUM_THREADS` environment variable or
+  the `--threads` flag when starting Julia.
+- `fit_kwargs` : a `Dict` controlling model fitting for each bootstrap sample, 
+  passed to `sem_fit`
+- `replace_kwargs`: a `Dict` passed to `replace_observed`
+
+# Example
+```julia
+# 1000 boostrap samples, fitted with :NLopt
+using NLopt
+
+se_bootstrap(
+    fitted;
+    n_boot = 1000,
+    engine = :NLopt,
+)
+```
+"""
+function se_bootstrap(
+    fitted::SemFit;
+    n_boot = 3000,
+    data = nothing,
+    specification = nothing,
+    engine = :Optim,
+    parallel = false,
+    fit_kwargs = Dict(),
+    replace_kwargs = Dict()
+)
+    # access data and convert to matrix
+    data = prepare_data_bootstrap(data, fitted.model)
+    start = solution(fitted)
+    # pre-allocations
+    total_sum         = zero(start)
+    total_squared_sum = zero(start)
+    n_failed          = Ref(0)
+    n_conv            = Ref(0)
+    # fit to bootstrap samples
+    if !parallel
+        for _ in 1:n_boot
+            try
+                sample_data = bootstrap_sample(data)
+                new_model = replace_observed(
+                    fitted.model;
+                    data = sample_data,
+                    specification = specification,
+                    replace_kwargs...,
+                )
+                new_fit = fit(new_model; start_val = start, engine = engine, fit_kwargs...)
+                sol = solution(new_fit)
+                conv = converged(new_fit)
+                if conv
+                    n_conv[] += 1
+                    @. total_sum         += sol
+                    @. total_squared_sum += sol^2
+                end
+            catch
+                n_failed[] += 1
+            end
+        end
+    else
+        n_threads = Threads.nthreads()
+        # Pre-create one independent model copy per thread via deepcopy.
+        model_pool = Channel(n_threads)
+        for _ in 1:n_threads
+            put!(model_pool, deepcopy(fitted.model))
+        end
+        # fit models in parallel
+        lk = ReentrantLock()
+        Threads.@threads for _ in 1:n_boot
+            thread_model = take!(model_pool)
+            try
+                sample_data = bootstrap_sample(data)
+                new_model = replace_observed(
+                    thread_model;
+                    data = sample_data,
+                    specification = specification,
+                    replace_kwargs...,
+                )
+                new_fit = fit(new_model; start_val = start, engine = engine, fit_kwargs...)
+                sol = solution(new_fit)
+                conv = converged(new_fit)
+                if conv
+                    lock(lk) do
+                        n_conv[] += 1
+                        @. total_sum         += sol
+                        @. total_squared_sum += sol^2
+                    end
+                end
+            catch
+                lock(lk) do
+                    n_failed[] += 1
+                end
+            finally
+                put!(model_pool, thread_model)
+            end
+        end
+    end
+    # compute parameters
+    n_conv = n_conv[]
     sd = sqrt.(total_squared_sum / n_conv - (total_sum / n_conv) .^ 2)
     if !iszero(n_failed[])
-        @warn "During bootstrap sampling, "*string(n_failed[])*" models did not converge"
+        @warn "During bootstrap sampling, "*string(n_failed[])*" samples errored"
     end
+    @info string(n_conv)*" models converged"
     return sd
 end
+
+############################################################################################
+### Helper Functions
+############################################################################################
 
 function bootstrap_sample(data::Matrix)
     nobs = size(data, 1)
