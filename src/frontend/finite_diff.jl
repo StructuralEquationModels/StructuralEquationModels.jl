91d6f4774acbae0f7ed4cb5b40a39fdf26a259bf
Alexey Stukalov <astukalol@seer.bio>
2026-03-30 15:51:19 -0700

    finite_diff: replace_observed()
    calls replace_observed() for the underlying term

--- src/frontend/finite_diff.jl
+++ src/frontend/finite_diff.jl
@@ -2,12 +2,26 @@ _unwrap(wrapper::SemFiniteDiff) = wrapper.model
 params(wrapper::SemFiniteDiff) = params(wrapper.model)
 loss_terms(wrapper::SemFiniteDiff) = loss_terms(wrapper.model)
 
+replace_observed(wrapper::SemFiniteDiff, data) =
+    SemFiniteDiff(replace_observed(wrapper.model, data))
+
 FiniteDiffLossWrappers = Union{LossFiniteDiff, SemLossFiniteDiff}
 
 _unwrap(term::AbstractLoss) = term
 _unwrap(wrapper::FiniteDiffLossWrappers) = wrapper.loss
 implied(wrapper::FiniteDiffLossWrappers) = implied(_unwrap(wrapper))
 observed(wrapper::FiniteDiffLossWrappers) = observed(_unwrap(wrapper))
+
+replace_observed(wrapper::LossFiniteDiff, data) =
+    LossFiniteDiff(replace_observed(_unwrap(wrapper), data))
+
+replace_observed(wrapper::SemLossFiniteDiff, new_observed::SemObserved) =
+    SemLossFiniteDiff(replace_observed(_unwrap(wrapper), new_observed))
+
+replace_observed(
+    wrapper::SemLossFiniteDiff,
+    data::Union{AbstractMatrix, DataFrame},
+) = SemLossFiniteDiff(replace_observed(_unwrap(wrapper), data))
 
 FiniteDiffWrapper(model::AbstractSem) = SemFiniteDiff(model)
 FiniteDiffWrapper(loss::AbstractLoss) = LossFiniteDiff(loss)
