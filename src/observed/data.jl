9381ae30e9074496c79b2990559eff8ecefe4cb0
Alexey Stukalov <astukalov@gmail.com>
2026-05-11 18:27:35 -0700

    declare cov matrices symmetric

--- src/observed/data.jl
+++ src/observed/data.jl
@@ -23,10 +23,10 @@ For observed data without missings.
 - `obs_cov(::SemObservedData)` -> observed covariance matrix
 - `obs_mean(::SemObservedData)` -> observed mean vector
 """
-struct SemObservedData{D <: Union{Nothing, AbstractMatrix}, S <: Number} <: SemObserved
+struct SemObservedData{D <: Union{Nothing, AbstractMatrix}, C, S <: Number} <: SemObserved
     data::D
     observed_vars::Vector{Symbol}
-    obs_cov::Matrix{S}
+    obs_cov::C
     obs_mean::Vector{S}
     nsamples::Int
 end
@@ -58,7 +58,7 @@ function SemObservedData(;
         throw
     end
 
-    return SemObservedData(data, obs_vars, obs_cov, vec(obs_mean), size(data, 1))
+    return SemObservedData(data, obs_vars, Symmetric(obs_cov), vec(obs_mean), size(data, 1))
 end
 
 ############################################################################################
