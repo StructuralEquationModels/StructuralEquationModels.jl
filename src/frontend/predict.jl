b19625aecf5f6f4a81971d656e1dfa04d4154a5e
Alexey Stukalov <astukalol@seer.bio>
2026-05-11 18:27:36 -0700

    predict: add alpha regularization

--- src/frontend/predict.jl
+++ src/frontend/predict.jl
@@ -60,16 +60,21 @@ function latent_scores_operator(
     ::SemRegressionScores,
     implied::SemImplied,
     A::AbstractMatrix,
-    S::AbstractMatrix,
+    S::AbstractMatrix;
+    alpha::Number = 0,
 )
     implied = SEM.implied(model)
     ram = implied.ram_matrices
     lv_inds = latent_var_indices(ram)
 
     lv_FA = ram.F * A[:, lv_inds]
-    lv_I_A⁻¹ = inv(I - A)[lv_inds, :]
 
-    cov_lv = X_A_Xt(S, lv_I_A⁻¹)
+    cov_lv = if alpha == 0
+        lv_I_A⁻¹ = inv(I - A)[lv_inds, :]
+        X_A_Xt(S, lv_I_A⁻¹)
+    else
+        inv(Xt_A_X(inv(S), I - A) + alpha * I)[lv_inds, lv_inds]
+    end
     Σ = implied.Σ
     Σ⁻¹ = inv(Σ)
     return cov_lv * lv_FA' * Σ⁻¹
@@ -79,7 +84,8 @@ function latent_scores_operator(
     ::SemBartlettScores,
     implied::SemImplied,
     A::AbstractMatrix,
-    S::AbstractMatrix,
+    S::AbstractMatrix;
+    alpha::Number = 0,
 )
     ram = implied.ram_matrices
     lv_inds = latent_var_indices(ram)
@@ -88,6 +94,7 @@ function latent_scores_operator(
     obs_inds = observed_var_indices(ram)
     ov_S⁻¹ = inv(S[obs_inds, obs_inds])
     cov_lv⁻¹ = Xt_A_X(ov_S⁻¹, lv_FA)
+    (alpha != 0) && (cov_lv⁻¹ += alpha * I)
     cov_lv = inv(cov_lv⁻¹)
 
     return cov_lv * lv_FA' * ov_S⁻¹
@@ -97,7 +104,8 @@ function predict_latent_scores(
     method::SemScoresPredictMethod,
     model::SemLoss,
     params::AbstractVector,
-    data::SemObserved = observed(model),
+    data::SemObserved = observed(model);
+    alpha::Number = 0,
 )
     nobserved_vars(data) == nobserved_vars(model) || throw(
         DimensionMismatch(
@@ -109,17 +117,17 @@ function predict_latent_scores(
             "The length of parameters vector ($(length(params))) does not match the number of parameters in the model ($(nparams(model)))",
         ),
     )
+    (alpha < 0) &&
+        throw(ArgumentError("The regularization parameter alpha must be non-negative"))
 
     implied = SEM.implied(model)
     ram = implied.ram_matrices
     update!(EvaluationTargets(0.0, nothing, nothing), implied, params)
-    ram = implied.ram_matrices
-    lv_inds = latent_var_indices(ram)
 
     A = materialize(ram.A, params)
     S = materialize(ram.S, params)
     lv_inds = latent_var_indices(ram)
-    lv_scores_op = latent_scores_operator(method, implied, A, S)
+    lv_scores_op = latent_scores_operator(method, implied, A, S; alpha)
 
     data =
         data.data .- (isnothing(data.obs_mean) ? mean(data.data, dims = 1) : data.obs_mean')
@@ -139,4 +147,5 @@ predict_latent_scores(
     params::AbstractVector,
     data::SemObserved = observed(model);
     method::Symbol = :regression,
-) = predict_latent_scores(SemScoresPredictMethod(method), model, params, data)
+    alpha::Number = 0,
+) = predict_latent_scores(SemScoresPredictMethod(method), model, params, data; alpha)
